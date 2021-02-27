db51dedb4df455a485d62f33cbc37b62b22f105d
Kabeer <mozammil83200@gmail.com>
2021-02-27 16:41:53 +0530

    saved all file

---
+++ js/dom-main.js
@@ -0,0 +1,23 @@
+// DOM Targetting Methods...............
+
+// var element;
+// element=document;
+// element=document.all[10];
+// element=document.all[12]
+// element=document.head
+// element=document.links;
+// element=document.forms;
+// element=document.title;
+// element=document.doctype;
+// element=document.url;   eg http://127.0.0.1:5500/index2.html
+// element=document.domain;  eg mysir g.com
+// element=document.baseURI;   returm comple url of our websites
+// element=document.getElementById("navbar");
+// element=document.getElementsByClassName("box");
+// element=document.getElementsByTagName("h1");
+// element=document.getElementsByClassName("box");
+// element=document.getElementsByClassName("box")[1];
+// 
+// element=document.getElementsByTagName("p")[1];
+
+// console.log(element);
---
+++ js/getset.js
@@ -0,0 +1,4 @@
+var a;
+a=document.getElementByTagName("section").innerText
+;
+console.log(a);
